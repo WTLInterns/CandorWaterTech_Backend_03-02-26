7b695c03b3ca2a8408eb77fba69e20495c33f4b6
WTL Interns <wtlinterns2@gmail.com>
2026-02-03 09:55:50 +0000

    firstUpdates

--- src/main/java/com/fieldforcepro/FieldForceProApplication.java
+++ src/main/java/com/fieldforcepro/FieldForceProApplication.java
@@ -2,10 +2,8 @@ package com.fieldforcepro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 
 @SpringBootApplication
-@EnableSpringHttpSession
 public class FieldForceProApplication {
 
     public static void main(String[] args) {
